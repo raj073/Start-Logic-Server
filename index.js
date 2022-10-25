137469eb178c29443f63f0431ea6d7df3bd578a3
raj073 <rajoni.das@outlook.com>
2022-10-25 12:35:36 +0600

    course and course details added

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/course.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('');
+    res.send('Start Logic Platform is Running.');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 
 
 app.listen(port, () => {
-    console.log('Start Logic Server Running on PORT:', port);
+    console.log('Dragon News Portal Server Running on PORT:', port)
 })
