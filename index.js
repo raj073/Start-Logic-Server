7d33099119fc2fac830bd377a7fad19cb47f29d1
raj073 <rajoni.das@outlook.com>
2022-10-25 11:49:58 +0600

    port added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('');
+});
+
+
+app.listen(port, () => {
+    console.log('Start Logic Platform Server Running on PORT:', port);
 })
