23941203d17e47f4a40f1497823e477bdd244f01
raj073 <rajoni.das@outlook.com>
2022-10-25 11:55:35 +0600

    port and cors updated

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log('Start Logic Platform Server Running on PORT:', port);
+    console.log('Start Logic Server Running on PORT:', port);
 })
